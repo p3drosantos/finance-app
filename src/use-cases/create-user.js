e4ec6cfb6aace2da18ff4dd834a9bb52a24e0d7d
Pedro Santos <P3dro.santosc@gmail.com>
2023-08-21 22:25:41 -0300

    feat: add PostgresCreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcryptjs'
+
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        //gerar senha do user
+        const userId = uuidv4()
+
+        //criptografar senha
+        const passwordHash = await bcrypt.hash(createUserParams.password, 10)
+
+        //inserir o usuario no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: passwordHash,
+        }
+
+        //chamar o repositorio
+        const PostgresCreateUseRepository = new PostgresCreateUserRepository()
+
+        const createdUser = await PostgresCreateUseRepository.execute(user)
+
+        return createdUser
+    }
+}
